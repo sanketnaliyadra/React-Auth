d5cd9fd39e2916598109a53d2c55861975c3192e
sanketnaliyadra <sanketnaliyadra00@gmail.com>
2024-01-16 00:07:47 -0500

    Added Redux Toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import { Link,useNavigate} from "react-router-dom";
+import { signInStart, signInFailure, signInSuccess } from "../redux/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 const SignIn = () => {
   const [formData, setFromData] = useState({});
-  const[loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+ const {loading, error} = useSelector((state) => state.user);
 
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleChange = (e) =>{
   // Spread(...) operator to keep the previous value of form data
       setFromData({...formData,[e.target.id]: e.target.value});
@@ -16,8 +18,7 @@ const SignIn = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
-      setError(false)
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: {
@@ -27,16 +28,16 @@ const SignIn = () => {
       });
       // console.log("res===",res)
       const data = await res.json();
-      setLoading(false)
-      if(data.success == false){
-        setError(true);
+      
+      if(data.success === false){
+        dispatch(signInFailure(data));
         return;
       }
+      dispatch(signInSuccess(data))
       navigate('/')
       // console.log(data);
     } catch (error) {
-      setLoading(false);
-      setError(true)
+      dispatch(signInFailure(error));
     }
   }
   return (
@@ -67,7 +68,7 @@ const SignIn = () => {
           <span className="text-blue-500">SignUp</span>
         </Link>
       </div>
-      <p className="text-red-600 mt-5">{error && 'Something went wrong !!'}</p>
+      <p className="text-red-600 mt-5">{error ? error.message || 'Something went wrong !!' : ''}</p>
     </div>
   );
 };
