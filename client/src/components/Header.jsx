98f156632dbaa50fc2d2e9b90807b75b2e4c2d3c
sanketnaliyadra <sanketnaliyadra00@gmail.com>
2024-01-17 14:25:45 -0500

    completed profile page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,16 +1,29 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import {useSelector} from 'react-redux';
 
 const Header = () => {
+  const {currentUser} = useSelector((state) => state.user)
   return (
     <div className='bg-slate-200'>
-      <div className="flex justify-between items-center max-w-6xl mx-auto py-4">
-        <Link><h1 className='font-bold'>React Auth</h1></Link>
+      <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+        <Link to='/'>
+          <h1 className='font-bold'>Auth App</h1>
+        </Link>
         <ul className='flex gap-4'>
-          <Link to="/"><li>Home</li></Link>
-          <Link to="/about"><li>About</li></Link>
-          <Link to="sign-in"><li>SignIn</li></Link>
-          <Link to="sign-up"><li>SignUp</li></Link>
+          <Link to='/'>
+            <li>Home</li>
+          </Link>
+          <Link to='/about'>
+            <li>About</li>
+          </Link>
+          <Link to='/profile'>
+            {currentUser ? (
+              <img src={currentUser.profilePicture} alt='profile' className='h-7 w-7 rounded-full object-cover' />
+            ) : (
+              <li>Sign In</li>
+            )}
+          </Link>
         </ul>
       </div>
     </div>
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Profile = () => {
+  const {currentUser} = useSelector((state) => state.user)
+  // console.log(currentUser)
   return (
-    <div>Profile</div>
+    <div className='max-w-lg p-3 mx-auto'>
+      <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
+      <form className='flex flex-col gap-4'>
+        <img src={currentUser.profilePicture} alt="profile-picture" className='h-24 w-24 self-center cursor-pointer rounded-full object-cover mt-2 mb-5'/>
+        <input defaultValue={currentUser.username} type="text" id='username' placeholder='Username' className='bg-slate-100 rounded-lg p-3'/>
+        <input defaultValue={currentUser.email} type="email" id='email' placeholder='Email' className='bg-slate-100 rounded-lg p-3'/>
+        <input type="password" id='password' placeholder='Password' className='bg-slate-100 rounded-lg p-3'/>
+        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-75'>update</button>
+      </form>
+      <div className='flex justify-between mt-5'>
+        <span className='text-red-600 cursor-pointer'>Delete Account</span>
+        <span className='text-red-600 cursor-pointer'>Sign Out</span>
+      </div>
+
+    </div>
   )
 }
 
